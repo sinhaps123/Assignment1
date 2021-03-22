a4e8a5573a8d14e85ffe1a154cab9f541034a366
sinhaps123 <98prachisinha@gmail.com>
2021-03-22 14:24:46 +0530

    hh

--- pika.go
+++ pika.go
@@ -3,20 +3,18 @@ package main
 import "fmt"
 
 func main() {
-<<<<<<< HEAD
-<<<<<<< HEAD
-	fmt.Printf("  god")
+
+	fmt.Printf(" assignment 1")
 	fmt.Printf(" World")
 	fmt.Printf(" Change Local")
 	fmt.Printf("Happy Monday")
-=======
-=======
+
 	fmt.Printf("sunday")
 	fmt.Printf("ohhh myyy god")
->>>>>>> b2
+
 	fmt.Printf("Hello World")
 	fmt.Printf("Fisrt Change Local")
 	fmt.Printf("Happy Mondaygit")
 	fmt.Printf("sun")
->>>>>>> b1
+
 }
